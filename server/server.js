b930532a7fb0049d411b46b615f4a6edd2a1b219
shawn <787990963@qq.com>
2017-12-18 21:18:47 +0800

    开发时服务器渲染

--- server/server.js
+++ server/server.js
@@ -2,19 +2,22 @@ const express = require('express')
 const ReactSSR = require('react-dom/server')
 const fs = require('fs')
 const path = require('path')
-
-const serverEntry = require('../dist/server-entry').default
-
-let template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
-
 const app = express()
+const isDev = process.env.NODE_ENV === 'development'
+if (!isDev) {
+    const serverEntry = require('../dist/server-entry').default
+    const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+    app.use('/public', express.static(path.join(__dirname, '../dist')))
+    // 通过 Express 内置的 express.static 可以方便地托管静态文件，例如图片、CSS、JavaScript 文件等。
+    app.get('*', function (req, res) {
+        const appString = ReactSSR.renderToString(serverEntry)
+        res.send(template.replace('<app></app>', appString))
+    })
+} else {
+    const devStatic = require('./utils/dev-static');
+    devStatic(app)
+}
 
-app.use('/public',express.static(path.join(__dirname,'../dist')))
-// 通过 Express 内置的 express.static 可以方便地托管静态文件，例如图片、CSS、JavaScript 文件等。
-app.get('*', function (req, res) {
-    const appString = ReactSSR.renderToString(serverEntry)
-    res.send(template.replace('<app></app>', appString))
-})
 app.listen(3333, function () {
     console.log('server is listening on 3333')
 })
