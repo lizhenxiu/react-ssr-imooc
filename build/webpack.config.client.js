a97ea65d2fce878f3d495862dbf35b3524b98c60
shawn <787990963@qq.com>
2017-12-18 18:11:29 +0800

    react热加载

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,13 +1,13 @@
 const path = require('path');
+const webpack = require('webpack')
 const htmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-    entry: {
-        app: path.join(__dirname, '../client/app.js')
-    },
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
     output: {
         filename: '[name].[hash].js',
         path: path.join(__dirname, '../dist'),
-        publicPath: '/public'
+        publicPath: '/public/'
     },
     module: {
         rules: [
@@ -30,3 +30,33 @@ module.exports = {
         })
     ]
 }
+
+// localhost:8888/filename
+if (isDev) {
+    config.entry = {
+        app: [
+            'react-hot-loader/patch',
+            path.join(__dirname, '../client/app.js')
+        ]
+    }
+    config.devServer = {
+        host: '0.0.0.0',
+        compress: true,
+        port: '8888',
+        contentBase: path.join(__dirname, '../dist'),
+        hot: true,
+        overlay: { // 语法错误时，页面出现黑色背景
+            errors: true
+        },
+        publicPath: '/public/',// 对应output 的publicPath
+        historyApiFallback: {
+            index: '/public/index.html'
+        },
+        proxy: {
+            '/api': 'http://localhost:3333'
+        }
+    }
+    config.plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
+module.exports = config
--- client/app.js
+++ client/app.js
@@ -1,5 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import App from './App.jsx'
-
-ReactDOM.hydrate(<App />, document.getElementById('root'))
+const root = document.getElementById('root')
+const render = Components => {
+    ReactDOM.hydrate(
+        <AppContainer>
+            <Components />
+        </AppContainer>, root
+    )
+}
+render(App)
+// Webpack Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('./App.jsx', () => {
+        const NewRoot = require('./App.jsx').default;
+        render(
+            NewRoot
+        );
+    })
+}
